8cac4ab7f1a73c8804846f854454ded8d3ae4951
Razz4780 <msmolarekg@gmail.com>
2026-08-03 19:35:25 +0200

    Made Miri happy

--- src/queue.rs
+++ src/queue.rs
@@ -212,23 +212,29 @@ impl<T> Drop for Queue<T> {
 
         impl<T> Drop for Guard<T> {
             fn drop(&mut self) {
-                // If one of the nodes panics on drop, `inner` guard will continue the work.
-                // If we're already in `inner` the process will be aborted.
-                // This behavior is the same as std LinkedList.
                 let mut inner = Self {
                     stub: self.stub,
                     tail: self.tail,
                 };
 
                 while inner.tail.is_null().not() {
+                    // `tail` may contain an old pointer tag for the embedded
+                    // stub. Use the fresh pointer created inside Queue::drop.
+                    // This makes Miri happy.
+                    if std::ptr::eq(inner.tail, inner.stub) {
+                        inner.tail = unsafe { &*inner.stub }
+                            .next_enqueued
+                            .load(Ordering::Acquire);
+                        continue;
+                    }
+
                     let next = unsafe { &*inner.tail }
                         .next_enqueued
                         .load(Ordering::Acquire);
                     let to_drop = inner.tail;
                     inner.tail = next;
-                    if to_drop != inner.stub {
-                        let _ = unsafe { Arc::from_raw(to_drop) };
-                    }
+
+                    let _ = unsafe { Arc::from_raw(to_drop) };
                 }
 
                 let _ = ManuallyDrop::new(inner);
